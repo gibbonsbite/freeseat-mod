12fc748a9079822a2b7ad47a6bc2820753372853
gibbonsbite <toni.kivirinne@gmail.com>
2015-02-12 03:04:33 +0200

    Enable 4 prices for movies

--- plugins/showedit/index.php
+++ plugins/showedit/index.php
@@ -514,9 +514,11 @@ echo '</div><div class="form">';
 echo '<h3>' . $lang['prices'] . '</h3>';
 echo '<table BORDER="1" CELLPADDING="4" >';
 echo '<tr><th>'. $lang["class"] . '<th>'.$lang["price"].'<th>'.$lang["price_discount"].'<th>'.$lang["comment"].'</tr>';
-for ( $i=1; $i<=2; $i++ ) { // class loop
+for ( $i=1; $i<=4; $i++ ) { // class loop
 	if ($i==1) echo '<tr><td>Norm</td>';
 	if ($i==2) echo '<tr><td>3D</td>';
+	if ($i==3) echo '<tr><td>Ale1</td>';
+	if ($i==4) echo '<tr><td>Ale2</td>';
 	//echo '<tr><td>'. $i . '</td>';
 	for ( $j=CAT_NORMAL; $j>=CAT_REDUCED; $j-- ) { // cat loop
 		echo '<td>';
