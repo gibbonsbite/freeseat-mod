5bd2c0613ba83fda787d33886c71cb61946a5343
gibbonsbite <toni.kivirinne@gmail.com>
2015-09-29 17:43:47 +0300

    Add finnish language to castpw plugin

---
+++ plugins/castpw/languages/finnish.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['castpw_length'] = "Salasanan täytyy olla vähintään 8 merkkiä pitkä";
+$lang['castpw_saved'] = "Salasana tallennettu";
+$lang['castpw_incorrect'] = "Väärä salasana";
+$lang['castpw_prompt'] = "Aseta uusi salasana ";
+$lang['castpw_input'] = "Salasana ";
+$lang['castpw_help'] = <<<EOD
+Use letters and numbers only, at least 8 characters long. 
+Spaces and punctuation will be ignored. 
+Example: "Mother&#39;s Day" will become "MothersDay"
+EOD;
+
+?>
